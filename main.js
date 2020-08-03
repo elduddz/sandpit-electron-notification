9c5681e644748d4ea1742b0c8801a7c33241f28f
ElDuddz <elduddz@outlook.com>
2020-08-04 00:26:34 +0100

    Working on Notice

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 const { app, BrowserWindow, Notification } = require('electron')
-
+const { path } = require('path')
 const { fork } = require('child_process')
 const ps = fork(`${__dirname}/server.js`)
 
-function createWindow() {
-    // Create the browser window.
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-
-    // and load the index.html of the app.
-    win.loadFile('index.html')
-}
-
 app.whenReady().then(() => {
-    createWindow()
 })
